2a34c0e6a5970d36cf947cc84a9bfb936dbdc0e1
Lucas Sandoval <sandovalucas88@gmail.com>
2021-11-28 18:32:38 -0300

    Creamos el modelo de formulario en 'app.component.ts' que va a estar asociado a nuestro formulario en la vista html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formulariodecontacto-sandoval';
+  form: FormGroup;
+
+
+  constructor() {
+    this.form = new FormGroup({
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.minLength(5)]),
+      apellido: new FormControl(''),
+      email: new FormControl('', Validators.email),
+      telefono: new FormControl('', Validators.minLength(8)),
+      comentario: new FormControl(''),
+    });
+  }
+  submit(){
+    console.log()
+  }
 }
